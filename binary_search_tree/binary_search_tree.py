a9be686e54ff4124663a580ed9372e8370fcda33
Gill Abada <gill@callifi.com>
2019-06-12 16:04:17 -0400

    complete for_each.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -46,4 +46,8 @@ class BinarySearchTree:
 
 
   def for_each(self, cb):
-    pass
+    cb(self.value)
+    if self.left:
+      self.left.for_each(cb)
+    if self.right:
+      self.right.for_each(cb)
